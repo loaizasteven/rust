fd4142227878ec278f9224aa60c1f2eb935f8360
Steven Loaiza <steven.loaiza@outlook.com>
2024-12-21 15:29:10 -0500

    Move files to src

--- hello_world/main.rs
+++
@@ -1,8 +0,0 @@
-//  source file main.rs, convention is separate words with underscores
-//  print hello world to the console
-// rustc main.rs to compile
-// run with ./main on macOS or .\main.exe on windows
-
-fn main() {
-    println!("Hello, world!");
-}
