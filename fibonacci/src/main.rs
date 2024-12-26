b70e4b30270a1c877a64b67e806fd4a9f5d6f457
Steven Loaiza <steven.loaiza@outlook.com>
2024-12-26 13:25:04 -0500

    Allow user input

--- fibonacci/src/main.rs
+++ fibonacci/src/main.rs
@@ -1,3 +1,5 @@
+use std::io;
+
 fn fib(n: i32) -> i32 {
     if n == 0 {
         return 0;
@@ -9,6 +11,11 @@ fn fib(n: i32) -> i32 {
 }
 
 fn main() {
-    let response = fib(4);
-    println!("Answer: {response}");
+    let mut n = String::new();
+
+    println!("Enter a number: ");
+    io::stdin().read_line(&mut n).expect("Failed to read line");
+    let n: i32 = n.trim().parse().expect("Please type a number");
+    let response = fib(n);
+    println!("Fib Sequence Results for '{n}' is {response}");
 }
