789af248eaa7e966f4df0d2997e58a20a573f340
Steven Loaiza <steven.loaiza@outlook.com>
2024-12-27 19:48:43 -0500

    Add func to find index of first word

--- references/src/main.rs
+++ references/src/main.rs
@@ -4,6 +4,10 @@ fn main() {
     mutable_reference(&mut s1);
     reference(&s1);
     println!("Value of s1 is still valid: {s1}"); // s1 is still valid here
+
+    let word = "Hello World";
+    let index = first_word(&word.to_string());
+    println!("Index of first word is: {index}");
 }
 
 fn reference(s: &String){
@@ -13,4 +17,16 @@ fn reference(s: &String){
 fn mutable_reference(s2: &mut String){
     s2.push_str("_mutable");
     println!("Value of s2 is mutable reference of s, {s2}") // s2 chnages the value of s from the main() scope 
+}
+
+fn first_word(s: &String) -> usize {
+    let bytes = s.as_bytes(); // elements of string as bytes e.g. "Hello" -> [72, 101, 108, 108, 111] for signed ASCII values 
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' { // b' ' is byte literal for space otherwise use signed ASCII value 32 
+            return i;
+        }
+    }
+
+    s.len()
 }
