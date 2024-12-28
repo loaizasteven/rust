8d453709a0f92c7b27517e51acacae26574e0834
Steven Loaiza <steven.loaiza@outlook.com>
2024-12-27 21:38:15 -0500

    Set to intentially not used to suppress warn

--- struct_types/src/main.rs
+++ struct_types/src/main.rs
@@ -2,19 +2,19 @@ fn main() {
     struct User{
         name: String,
         email: String,
-        active: Option<bool>,
+        _active: Option<bool>,
     }
 
     let user1 = User{
         name: String::from("John Doe"),
         email: String::from("JDoe@email.com"),
-        active: None,
+        _active: None,
     };
 
     let _user2 = User{
         name: String::from("Jane Doe"),
         email: String::from("JaDoe@email.com"),
-        active: Some(true), // When setting the value need to specific Some(value_type) or None, due to the Option<> type
+        _active: Some(true), // When setting the value need to specific Some(value_type) or None, due to the Option<> type
     };
 
     println!("User 1 name is {0}, email is {1}", user1.name, user1.email);
