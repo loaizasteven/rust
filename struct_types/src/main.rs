26817749fe5fd2023ec4d5d43076e4651c4ced19
Steven Loaiza <steven.loaiza@outlook.com>
2024-12-28 17:02:34 -0500

    Add derive(debug) to struct to print to console

--- struct_types/src/main.rs
+++ struct_types/src/main.rs
@@ -1,4 +1,5 @@
-struct User{
+#[derive(Debug)] // This is a derive attribute that allows us to print the struct using the {:?} formatter
+struct User {
     name: String,
     email: String,
     _active: Option<bool>,
@@ -6,15 +7,15 @@ struct User{
 
 fn main() {
     // Structs are similar to tuples, but with named fields
-    fn build_user(name: String, email: String) -> User{
-        User{
+    fn build_user(name: String, email: String) -> User {
+        User {
             name, // With field init shorthand syntax, we can use the variable name as the field name
             email,
             _active: Some(true), // When setting the value need to specific Some(value_type) or None, due to the Option<> type
         }
     }
 
-    let user1 = User{
+    let user1 = User {
         name: String::from("John Doe"),
         email: String::from("JDoe@email.com"),
         _active: None,
@@ -22,7 +23,7 @@ fn main() {
 
     let mut user2 = build_user(String::from("Jane Doe"), String::from("JDoe@email.com"));
     // Duplicate user2 using struct update syntax
-    let _user2_updated = User{
+    let _user2_updated = User {
         _active: Some(false),
         ..user2 // specify remaining fields to be moved/copied from user2
     };
@@ -30,4 +31,5 @@ fn main() {
     // println!("User 2 email is {0}", user2.email); // This will throw an error "value borrowed here after move"
     user2.email = String::from("JaDoe@email.com");
     println!("User 1 name is {0}, email is {1}", user1.name, user1.email);
+    println!("User 1 Object is: {user1:#?}"); // pretty print with #?
 }
