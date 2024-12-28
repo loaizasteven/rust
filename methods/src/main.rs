9444a7fa173ab19d1aa4bfaea0ff02960dc7f30e
Steven Loaiza <steven.loaiza@outlook.com>
2024-12-28 17:59:10 -0500

    Add method with self

---
+++ methods/src/main.rs
@@ -0,0 +1,22 @@
+// create a struct 
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+}
+
+fn main() {
+    let rect = Rectangle {
+        width: 30,
+        height: 50,
+    };
+
+    let _area_val = rect.area();
+    println!("Debugging the struct Rectangle: {rect:#?}");
+}
