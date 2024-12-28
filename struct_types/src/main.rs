b245a77f9d8fe3c212cf988d98668e5737af9004
Steven Loaiza <steven.loaiza@outlook.com>
2024-12-27 21:37:15 -0500

    Add struct type

---
+++ struct_types/src/main.rs
@@ -0,0 +1,21 @@
+fn main() {
+    struct User{
+        name: String,
+        email: String,
+        active: Option<bool>,
+    }
+
+    let user1 = User{
+        name: String::from("John Doe"),
+        email: String::from("JDoe@email.com"),
+        active: None,
+    };
+
+    let _user2 = User{
+        name: String::from("Jane Doe"),
+        email: String::from("JaDoe@email.com"),
+        active: Some(true), // When setting the value need to specific Some(value_type) or None, due to the Option<> type
+    };
+
+    println!("User 1 name is {0}, email is {1}", user1.name, user1.email);
+}
