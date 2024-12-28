527b2e9548c7df282956951b73bdbd5970bdd3f3
Steven Loaiza <steven.loaiza@outlook.com>
2024-12-27 19:57:58 -0500

    Return slice of String

--- references/src/main.rs
+++ references/src/main.rs
@@ -7,7 +7,7 @@ fn main() {
 
     let word = "Hello World";
     let index = first_word(&word.to_string());
-    println!("Index of first word is: {index}");
+    println!("The value of the first word is: {index}");
 }
 
 fn reference(s: &String){
@@ -19,14 +19,14 @@ fn mutable_reference(s2: &mut String){
     println!("Value of s2 is mutable reference of s, {s2}") // s2 chnages the value of s from the main() scope 
 }
 
-fn first_word(s: &String) -> usize {
+fn first_word(s: &String) -> String {
     let bytes = s.as_bytes(); // elements of string as bytes e.g. "Hello" -> [72, 101, 108, 108, 111] for signed ASCII values 
 
     for (i, &item) in bytes.iter().enumerate() {
         if item == b' ' { // b' ' is byte literal for space otherwise use signed ASCII value 32 
-            return i;
+            return s[..i].to_string()
         }
     }
 
-    s.len()
+    return s[..].to_string()
 }
