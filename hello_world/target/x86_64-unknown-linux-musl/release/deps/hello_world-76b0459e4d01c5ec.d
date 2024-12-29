890301b5709d231a6761b33a167d7d7e77555edf
Steven Loaiza <steven.loaiza@outlook.com>
2024-12-29 16:26:06 -0500

    Add linux binaries

---
+++ hello_world/target/x86_64-unknown-linux-musl/release/deps/hello_world-76b0459e4d01c5ec.d
@@ -0,0 +1,5 @@
+/Users/steven/git/rust/hello_world/target/x86_64-unknown-linux-musl/release/deps/hello_world-76b0459e4d01c5ec: src/main.rs
+
+/Users/steven/git/rust/hello_world/target/x86_64-unknown-linux-musl/release/deps/hello_world-76b0459e4d01c5ec.d: src/main.rs
+
+src/main.rs:
---
+++ hello_world/target/x86_64-unknown-linux-musl/release/hello_world.d
@@ -0,0 +1,1 @@
+/Users/steven/git/rust/hello_world/target/x86_64-unknown-linux-musl/release/hello_world: /Users/steven/git/rust/hello_world/src/main.rs
