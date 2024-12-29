b6acdf2798f1a571e32d6a7bfe5598ed972f9973
Steven Loaiza <steven.loaiza@outlook.com>
2024-12-29 16:01:37 -0500

    Add hello world binary

---
+++ hello_world/target/debug/deps/hello_world-798874a9f846d720.d
@@ -0,0 +1,5 @@
+/Users/steven/git/rust/hello_world/target/debug/deps/hello_world-798874a9f846d720: src/main.rs
+
+/Users/steven/git/rust/hello_world/target/debug/deps/hello_world-798874a9f846d720.d: src/main.rs
+
+src/main.rs:
---
+++ hello_world/target/release/deps/hello_world-94dfa810ac98a10a.d
@@ -0,0 +1,5 @@
+/Users/steven/git/rust/hello_world/target/release/deps/hello_world-94dfa810ac98a10a: src/main.rs
+
+/Users/steven/git/rust/hello_world/target/release/deps/hello_world-94dfa810ac98a10a.d: src/main.rs
+
+src/main.rs:
